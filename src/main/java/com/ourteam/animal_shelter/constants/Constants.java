f37637ea9ab6f8d02762fd5f5c45f2a5e7c74c41
IMBor1 <ilacerber1@gmail.com>
2024-06-16 18:03:48 +0300

    добавил зависимости телеграмма, ликвибэйс,сваггер,jpa, postres. пока не работает без создания базы и добавления токена

---
+++ src/main/java/com/ourteam/animal_shelter/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.ourteam.animal_shelter.constants;
+
+public class Constants {
+    public static String MEET = "Привет! Я телеграм бот, Петя! Я взаимодействую с приютами и помогаю найти дом для собак.";
+}
--- src/main/java/com/ourteam/animal_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/ourteam/animal_shelter/listener/TelegramBotUpdatesListener.java
@@ -1,8 +1,11 @@
 package com.ourteam.animal_shelter.listener;
 
+import com.ourteam.animal_shelter.constants.Constants;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,8 +30,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
+            try {
+
+                logger.info("Processing update: {}", update);
+                String comMsg = update.message().text();
+                if (comMsg.equalsIgnoreCase("/start")) {
+                    Long chatId = update.message().chat().id();
+                    SendResponse response = telegramBot.execute(new SendMessage(chatId, Constants.MEET));
+                }
+            } catch (Exception e) {
+                logger.error("update not correct");
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }

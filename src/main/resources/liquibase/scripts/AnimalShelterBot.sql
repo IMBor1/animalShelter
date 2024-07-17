a70b9ff2471618b49aee8da817b4854f93598032
Anton <anton22582258@gmail.com>
2024-07-17 21:48:18 +0300

    Изменил название метода в DogService. Оптимизировал файл sql.

--- src/main/resources/liquibase/scripts/AnimalShelterBot.sql
+++ src/main/resources/liquibase/scripts/AnimalShelterBot.sql
@@ -10,58 +10,24 @@ phone Text,
 timer timeStamp,
 probationary_period Integer);
 
-
---changeset anton22582258:create_shelter
-create table shelter
-(
-    id                                      bigserial primary key,
-    greeting                                text,
-    info                                    text,
-    dating_rules                            text,
-    adoption_documents                      text,
-    transportation_recommendations          text,
-    recommendations_arranging_baby          text,
-    recommendations_arranging_adult         text,
-    recommendations_arranging_with_features text
-);
 --changeset anton22582258:create_photo
 create table photo
 (
     id         bigserial primary key,
     file_size  bigint,
     media_type text,
-    path       text
+    path       text,
+    dog_id     bigint
 );
 
---changeset BIlya:1
-create table Client (
-id Serial,
-chat_Id Integer primary key,
-name Text,
-has_pet Boolean,
-phone Text,
-timer timeStamp,
-probationary_period Integer);
-
-
---changeset anton22582258:create_shelter
-create table shelter
+--changeset anton22582258:create_dog
+create table dog
 (
-    id                                      bigserial primary key,
-    greeting                                text,
-    info                                    text,
-    dating_rules                            text,
-    adoption_documents                      text,
-    transportation_recommendations          text,
-    recommendations_arranging_baby          text,
-    recommendations_arranging_adult         text,
-    recommendations_arranging_with_features text
-);
---changeset anton22582258:create_photo
-create table photo
-(
-    id         bigserial primary key,
-    file_size  bigint,
-    media_type text,
-    path       text
+    id                         bigserial primary key,
+    name                       varchar(255),
+    age                        int,
+    is_healthy                 boolean,
+    is_adopted                 boolean,
+    client_id                    bigint,
+    photo_id                     bigint
 );
